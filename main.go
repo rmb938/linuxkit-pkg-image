fc6d86ae8f21efd09bcba8f63e71a5078f01e77d
Ryan Belgrave <ryan@belgrave.me>
2020-01-25 15:31:07 -0600

    try anything

--- main.go
+++ main.go
@@ -39,10 +39,10 @@ func (m *middleFileReader) Read(p []byte) (int, error) {
 
 	log.Printf("Start: %v Size: %v Total: %v Read: %v", m.Start, m.Size, tmpTotal, n)
 
-	if uint32(tmpTotal) >= m.Size {
+	if uint32(tmpTotal) > m.Size {
 		log.Printf("Read more than size so we are done %v", int(m.Size-uint32(m.total)))
 		m.total = uint64(m.Size)
-		return int(m.Size - uint32(m.total)), nil
+		return int(m.Size - uint32(m.total)), io.EOF
 	}
 	m.total = m.total + uint64(n)
 
